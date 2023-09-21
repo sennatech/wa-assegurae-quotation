bd32ee53d411aab6bb33a94847957eab5de717a4
Bruno Farto <bruno.camargo@sennatech.com.br>
2023-09-21 17:47:33 -0300

    adicionando o name no DTO

--- src/main/java/br/com/sennatech/wasddoquotation/domain/Coverage.java
+++ src/main/java/br/com/sennatech/wasddoquotation/domain/Coverage.java
@@ -2,5 +2,5 @@ package br.com.sennatech.wasddoquotation.domain;
 
 import java.math.BigDecimal;
 
-public record Coverage(Integer code, BigDecimal value) {
+public record Coverage(Integer code,String name, BigDecimal value) {
 }
